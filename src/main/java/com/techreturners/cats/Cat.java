7d8bfeade4e4e47d7bab91244bb32d1204be2996
punkason <punkason@gmail.com>
2022-01-11 20:15:15 +0000

    add new classes

--- src/main/java/com/techreturners/cats/Cat.java
+++ src/main/java/com/techreturners/cats/Cat.java
@@ -3,4 +3,15 @@ package com.techreturners.cats;
 interface Cat {
 
 
+    void goToSleep();
+
+    boolean isAsleep();
+
+    void wakeUp();
+
+    String getSetting();
+
+    int getAverageHeight();
+
+    String eat();
 }
---
+++ src/main/java/com/techreturners/cats/LionCat.java
@@ -0,0 +1,33 @@
+package com.techreturners.cats;
+
+public class LionCat implements Cat {
+    @Override
+    public void goToSleep() {
+
+    }
+
+    @Override
+    public boolean isAsleep() {
+        return false;
+    }
+
+    @Override
+    public void wakeUp() {
+
+    }
+
+    @Override
+    public String getSetting() {
+        return null;
+    }
+
+    @Override
+    public int getAverageHeight() {
+        return 0;
+    }
+
+    @Override
+    public String eat() {
+        return null;
+    }
+}
